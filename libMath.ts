07526727d77ea883a3741ca7863a1c54175057b1
tree <tree@hugg.com.tw>
2023-10-06 16:23:39 +0800

    feat: add double

--- libMath.ts
+++ libMath.ts
@@ -13,3 +13,7 @@ export const multiply = (a: number, b: number) => {
 export const divide = (a: number, b: number) => {
     return a / b;
 }
+
+export const double = (n: number) => {
+    return n * 2;
+}
